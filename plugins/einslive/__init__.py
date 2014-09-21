3d8638828aea75e1cf803550b14c3b9d244c88f5
dasistdaniel <d.intrup@googlemail.com>
2014-09-21 18:18:01 +0200

    fix empty einslive page

--- plugins/einslive/__init__.py
+++ plugins/einslive/__init__.py
@@ -8,24 +8,26 @@ from datetime import date as dt
 def parse_playlist(data):
     playlist = []
     root = html.fromstring(data)
-    for x in range(1, 13):
-        date = dt.today().strftime('%Y-%m-%d')
-        time = root.xpath(
-            ".//*[@id='wsContentArea']/table/tbody/tr[" + str(x) + "]/td[1]/text()")[0]
-        artist = root.xpath(
-            ".//*[@id='wsContentArea']/table/tbody/tr[" + str(x) + "]/td[2]/text()")[0]
-        title = root.xpath(
-            ".//*[@id='wsContentArea']/table/tbody/tr[" + str(x) + "]/td[3]/text()")[0]
-        duration = ''
+    data = (root.xpath(".//*[@id='wsContentArea']/table/tbody/tr"))
+    if len(data) > 0:
+        for x in range(1, 13):
+            date = dt.today().strftime('%Y-%m-%d')
+            time = root.xpath(
+                ".//*[@id='wsContentArea']/table/tbody/tr[" + str(x) + "]/td[1]/text()")[0]
+            artist = root.xpath(
+                ".//*[@id='wsContentArea']/table/tbody/tr[" + str(x) + "]/td[2]/text()")[0]
+            title = root.xpath(
+                ".//*[@id='wsContentArea']/table/tbody/tr[" + str(x) + "]/td[3]/text()")[0]
+            duration = ''
 
-        try:
-            title = title.decode('unicode-escape').title()
-            artist = artist.decode('unicode-escape').title()
-        except:
-            f = open('error_delta', 'w')
-            f.write(data)
-            f.close()
+            try:
+                title = title.decode('unicode-escape').title()
+                artist = artist.decode('unicode-escape').title()
+            except:
+                f = open('error_delta', 'w')
+                f.write(data)
+                f.close()
 
-        playlist.append(
-            {'date': date, 'time': time, 'artist': artist, 'title': title, 'duration': duration})
+            playlist.append(
+                {'date': date, 'time': time, 'artist': artist, 'title': title, 'duration': duration})
     return playlist
