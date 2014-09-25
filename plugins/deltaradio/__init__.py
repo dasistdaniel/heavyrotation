1092d67a33bfacfd886fa664dd8a5323f4e5ab10
dasistdaniel <d.intrup@googlemail.com>
2014-09-25 19:56:05 +0200

    Deltaradio auf json umgestellt

--- plugins/deltaradio/__init__.py
+++ plugins/deltaradio/__init__.py
@@ -1,25 +1,19 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import feedparser
+import json
 import datetime
-from time import mktime
-
 
 def parse_playlist(data):
     playlist = []
-    root = feedparser.parse(data)
-    for entry in root.entries:
-        date = datetime.date.today().strftime('%Y-%m-%d')
-        time = datetime.datetime.fromtimestamp(
-            mktime(entry.published_parsed)).strftime('%H:%M:%S')
-        artist = entry.artist
-        title = entry.plain_title
-        duration = entry.duration
-
-        title = title.decode('unicode-escape').title()
-        artist = artist.decode('unicode-escape').title()
+    items = json.loads(data)
+    
+    for item in items:
+        artist = item['artist'].title()
+        title = item['plain_title'].title()
+        duration = item['duration']
+        date = datetime.datetime.fromtimestamp(item['date']).strftime('%Y-%m-%d')
+        time =  datetime.datetime.fromtimestamp(item['date']).strftime('%H:%M:%S')
         
-        playlist.append(
-            {'date': date, 'time': time, 'artist': artist, 'title': title, 'duration': duration})
+        playlist.append({'date': date, 'time': time, 'artist': artist, 'title': title, 'duration': duration})
     return reversed(playlist)
