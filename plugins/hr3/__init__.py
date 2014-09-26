18d1c4900a4396dbefcd1cc157b1d8c71549bd69
dasistdaniel <d.intrup@googlemail.com>
2014-09-26 22:31:48 +0200

    fix für hr3

--- plugins/hr3/__init__.py
+++ plugins/hr3/__init__.py
@@ -9,10 +9,9 @@ def parse_playlist(data):
     playlist = []
 
     root = html.fromstring(data)
-    #print data
     laenge = len(root.xpath('//tr'))
-    #print laenge
-    for x in range(2, laenge + 1):
+
+    for x in range(2, laenge -1 ):
         date_ = root.xpath("//tr["+str(x)+"]/td[1]/text()")[0].split('.')
         date = date_[2] + "-" + date_[1] + "-" + date_[0]
         time = root.xpath("//tr["+str(x)+"]/td[2]/text()")[0] + ':00'
